17b8eaa013a0e765b07dbfc476667aaa7b130af7
Diane Leigh <leighd2008@gmail.com>
2018-07-17 07:40:36 -0400

    merged with redux version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import { searchRobots, requestRobots } from './reducers';
+
+import './index.css';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+
+const store =
+  createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
