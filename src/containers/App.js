dd4617dac8f257e96b87edd4181f97ec71f1448f
Diane Leigh <leighd2008@gmail.com>
2018-07-17 06:26:56 -0400

    oops

--- src/containers/App.js
+++ src/containers/App.js
@@ -41,17 +41,18 @@ class App extends Component {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     return (
-        <div className='tc'>
-          <Header />
-          <SearchBox searchChange={onSearchChange}/>
-          <Scroll>
-            {isPending ? <h1>Loading</h1> :
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          { isPending ? <h1>Loading</h1> :
             <ErrorBoundry>
               <CardList robots={filteredRobots} />
-            </ErrorBoundry> }
-          </Scroll>
-        </div>
-      );
+            </ErrorBoundry>
+          }
+        </Scroll>
+      </div>
+    );
   }
 }
 
